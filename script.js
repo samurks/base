1affa2d3330fec202ce3513e02bdf453e77f9dbd
samurks <samueltorochagas@gmail.com>
2023-05-24 08:57:48 -0300

    adicionei a imagem do produto destaque
    manipulei os dados inseridos pelo JS usando o CSS centralizando e mudando a cor do background

--- script.js
+++ script.js
@@ -1,34 +1,39 @@
-class Produto{
-    constructor(nome, datadeCadastro, descricao, preco){
+class Produto {
+    constructor (nome, datadeCadastro, descricao, valor){
     this.nome = nome;
     this.datadeCadastro = datadeCadastro;
     this.descricao = descricao;
-    this.preco = preco;
-
+    this.valor = valor;
+  }
+  mostrarProdutos (){
+    return this.nome + this.datadeCadastro + this.descricao + this.valor ;
+  }
+  }
+  
+  const produto = new Produto("Vandal Neptuno", "31-02-1999", "Skin de Vandal - Valorant", "R$ 79,49");
+  console.log(produto.mostrarProdutos())
+  
+  class ProdutosDestaque extends Produto{
+    constructor (nome, datadeCadastro, descricao,  valor, imagemDestaque){
+      super(nome, datadeCadastro, descricao, valor, imagemDestaque)
+      this.imagemDestaque = imagemDestaque;
     }
-
-    mostrarProduto(){
-    return this.nome +  this.datadeCadastro  + this.descricao + this.preco;
-    }
-}
-
-class produtoDestaque extends Produto{
-    constructor(nome, datadeCadastro, descricao, preco, imagemDestaque){
-        super(nome, datadeCadastro, descricao, preco)
-        this.imagemDestaque = imagemDestaque;
-    }
-
-    mostrarProdutoDestaque(){
-    return ` 
-    <p class="vermeio">${this.nome}<p>
-    <p>${this.datadeCadastro}<p>
-        `
-}
-}
-
-   const produto = new Produto('manteiga', '21/07/2000', 'bom', 'R$5')
-     const produtodestaque = new produtoDestaque('margarina', '21/07/2020', 'otimo', 'R$8', 'margarina.jpg')
-     console.log(produto.mostrarProduto())
-     console.log(produtodestaque.mostrarProduto());
-     
- produtoDestaque.insertAdjacentHTML('afterbegin', produto.mostrarProdutoDestaque());
+    mostrarProdutosDestaque (){
+        return `
+        <h1 class = "nm1" >${this.nome}</h1>
+        <h4 class = "nm1">${this.datadeCadastro}</h4>
+        <img class="imgDestaque"src="${this.imagemDestaque}"></img>
+        <p class = "nm1">${this.descricao}</p>
+        <p class = "nm1">${this.valor}</p>
+        ` 
+       // return this.nome + this.datadeCadastro + this.descricao + this.valor + this.imagemDestaque;
+     }
+  }   
+  const produtosDestaque = new ProdutosDestaque("Vandal Reaver", "24/05/2023", "Skin de Vandal - Valorant", "R$ 74,49", "https://valorantinfo.com/images/br/vandal-saqueador-nivel-5variante-1-vermelha_valorant_full_skin_163596.webp");
+    
+  
+  const produtos = document.getElementById("produto-destaque")
+  produtos.insertAdjacentHTML('afterbegin', produtosDestaque.mostrarProdutosDestaque());
+  
+  
+  
